b6b86375ae1073fe7709bcda8c9b9a306f7d1a04
Adhyan <adhyan_srivastava95@yahoo.com>
2016-01-17 01:28:26 +0530

    Improved UI

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -20,6 +20,7 @@ import android.view.ViewGroup;
 import android.support.v7.widget.ShareActionProvider;
 import android.widget.TextView;
 
+import com.example.android.sunshine.app.data.DetailFragment;
 import com.example.android.sunshine.app.data.WeatherContract;
 
 public class DetailActivity extends ActionBarActivity {
@@ -57,111 +58,5 @@ public class DetailActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    public static class DetailFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
-        private static final String LOG_TAG = DetailFragment.class.getSimpleName();
-        private String FORECAST_SHARE_HASH_TAG = "#Sunshine";
-        private ShareActionProvider mShareActionProvider;
-        private String mForecast;
-        private static final int DETAIL_LOADER = 0;
-        private static final String[] FORECAST_COLUMNS = {
-                // In this case the id needs to be fully qualified with a table name, since
-                // the content provider joins the location & weather tables in the background
-                // (both have an _id column)
-                // On the one hand, that's annoying.  On the other, you can search the weather table
-                // using the location set by the user, which is only in the Location table.
-                // So the convenience is worth it.
-                WeatherContract.WeatherEntry.TABLE_NAME + "." + WeatherContract.WeatherEntry._ID,
-                WeatherContract.WeatherEntry.COLUMN_DATE,
-                WeatherContract.WeatherEntry.COLUMN_SHORT_DESC,
-                WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
-                WeatherContract.WeatherEntry.COLUMN_MIN_TEMP,
-        };
-
-        // These indices are tied to FORECAST_COLUMNS.  If FORECAST_COLUMNS changes, these
-        // must change.
-        static final int COL_WEATHER_ID = 0;
-        static final int COL_WEATHER_DATE = 1;
-        static final int COL_WEATHER_DESC = 2;
-        static final int COL_WEATHER_MAX_TEMP = 3;
-        static final int COL_WEATHER_MIN_TEMP = 4;
-        public DetailFragment() {
-        }
-
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            setHasOptionsMenu(true);
-            super.onCreate(savedInstanceState);
-        }
-
-        @Override
-        public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-            super.onActivityCreated(savedInstanceState);
-            getLoaderManager().initLoader(DETAIL_LOADER, null, this);
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
-            return rootView;
-        }
-
-        @Override
-        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-            super.onCreateOptionsMenu(menu, inflater);
-            inflater.inflate(R.menu.detailfragment, menu);
-            MenuItem menuItem = menu.findItem(R.id.menu_item_share);
-            mShareActionProvider =
-                    (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
-            if (mForecast != null) {
-                mShareActionProvider.setShareIntent(createShareForecastIntent());
-            }
-        }
-
-        public Intent createShareForecastIntent() {
-            Intent shareIntent = new Intent(Intent.ACTION_SEND);
-            shareIntent.setType("text/plain");
-            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
-            shareIntent.putExtra(Intent.EXTRA_TEXT, mForecast + FORECAST_SHARE_HASH_TAG);
-            return shareIntent;
-        }
-
-        @Override
-        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-            Intent intent = getActivity().getIntent();
-            return new CursorLoader(
-                    getActivity(),
-                    intent.getData(),
-                    FORECAST_COLUMNS,
-                    null,
-                    null,
-                    null
-            );
-        }
-
-        @Override
-        public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-            Log.v(LOG_TAG, "In onLoadFinished");
-            if (!data.moveToFirst()) { return; }
-            String dateString = Utility.formatDate(data.getLong(COL_WEATHER_DATE));
-            String weatherDescription = data.getString(COL_WEATHER_DESC);
-            boolean isMetric = Utility.isMetric(getActivity());
-            String high = Utility.formatTemperature(data.getDouble(COL_WEATHER_MAX_TEMP), isMetric);
-            String low = Utility.formatTemperature(data.getDouble(COL_WEATHER_MIN_TEMP), isMetric);
-            mForecast = String.format("%s - %s - %s/%s", dateString, weatherDescription, high, low);
-            TextView detailTextView = (TextView)getView().findViewById(R.id.detail_text);
-            detailTextView.setText(mForecast);
-
-            if (mShareActionProvider != null) {
-                mShareActionProvider.setShareIntent(createShareForecastIntent());
-            }
-        }
-
-        @Override
-        public void onLoaderReset(Loader<Cursor> loader) {
-
-        }
     }
 }
---
+++ app/src/main/java/com/example/android/sunshine/app/StethoApplication.java
@@ -0,0 +1,35 @@
+package com.example.android.sunshine.app;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+
+/**
+ * Created by adhyan on 15/1/16.
+ */
+public class StethoApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Create an InitializerBuilder
+        Stetho.InitializerBuilder initializerBuilder =
+                Stetho.newInitializerBuilder(this);
+
+        // Enable Chrome DevTools
+        initializerBuilder.enableWebKitInspector(
+                Stetho.defaultInspectorModulesProvider(this)
+        );
+
+        // Enable command line interface
+        initializerBuilder.enableDumpapp(
+                Stetho.defaultDumperPluginsProvider(this)
+        );
+
+        // Use the InitializerBuilder to generate an Initializer
+        Stetho.Initializer initializer = initializerBuilder.build();
+
+        // Initialize Stetho with the Initializer
+        Stetho.initialize(initializer);
+    }
+}
+
